bb14133b36b477b0b9d26fc3e40e2f40bc86438c
Danix1212 <107116963+Danix1212@users.noreply.github.com>
2022-06-23 21:53:38 +0200

    update 2

--- Programme/randomizer.py
+++ Programme/randomizer.py
@@ -1,5 +1,29 @@
 import random
 
+list1 = []
+list2 = []
+userNumber1 = []
+userNumber2 = []
+
+def userlists (arg):
+    global userNumber1
+    global userNumber2
+    
+    if arg == list1:
+        for i in range(0,10):
+            userNumber1.append(int(input('Add a number to the first list: ')))
+        arg = userNumber1
+        return arg
+    
+    elif arg == list2:
+        for i in range(0,10):
+            userNumber2.append(int(input('Add a number to the second list: ')))
+        arg = userNumber2
+        return arg
+        
+    
+    
+
 
 def randomNumber ():
     numList = []
@@ -10,6 +34,6 @@ def randomNumber ():
 
     return numList
 
-list1 = randomNumber()
-list2 = randomNumber()
+list1 = userlists(list1)
+list2 = userlists(list2)
 
